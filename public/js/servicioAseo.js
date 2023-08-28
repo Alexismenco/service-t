feacdac722356f0bc891b6ea17e7fef925e8017b
Alexis Mer <mencoalexis@gmail.com>
2023-08-28 00:56:06 -0400

    com

---
+++ public/js/servicioAseo.js
@@ -0,0 +1,54 @@
+const serviciosAseo = {
+    aseo: [
+      {
+        area: "40",
+        servicios: {
+          light: {
+            precio: 29900,
+            img: 'aseo-b1.png'
+          },
+          premium: {
+            precio: 59900,
+            img: 'aseo-b3.png'
+          },
+        },
+      },
+      {
+        area: "70",
+        servicios: {
+          light: {
+            precio: 34900,
+            img: 'aseo-p1.png'
+          },
+          plus: {
+            precio: 54900,
+            img: 'aseo-p2.png'
+          },
+          premium: {
+            precio: 69900,
+            img: 'aseo-p3.png'
+          },
+        },
+      },
+      {
+        area: "100",
+        servicios: {
+          light: {
+            precio: 39900,
+            img: 'aseo-pr1.png'
+          },
+          plus: {
+            precio: 59900,
+            img: 'aseo-pr2.png'
+          },
+          premium: {
+            precio: 74900,
+            img: 'aseo-pr3.png'
+          },
+        },
+      },
+    ],
+  };
+  
+  module.exports = serviciosAseo;
+  
